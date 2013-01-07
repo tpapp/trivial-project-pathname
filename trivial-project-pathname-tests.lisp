77e861a944a9cb00bf72d7ff7900613ed9ed83d1
tpapp <tkpapp@gmail.com>
2013-01-07 12:30:18 +0100

    Started skeleton for tests.

---
+++ trivial-project-pathname-tests.lisp
@@ -0,0 +1,6 @@
+;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
+
+(cl:defpackage #:trivial-project-pathname-tests
+  (:use #:cl #:alexandria #:anaphora #:clunit #:let-plus))
+
+(in-package #:trivial-project-pathname-tests)
