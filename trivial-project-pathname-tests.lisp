04c2ede6616902bd1b9d90bbd4d2e8d8be6c184b
tpapp <tkpapp@gmail.com>
2013-01-07 12:31:43 +0100

    Added function for running tests.

--- trivial-project-pathname-tests.lisp
+++ trivial-project-pathname-tests.lisp
@@ -1,6 +1,14 @@
 ;;; -*- Mode:Lisp; Syntax:ANSI-Common-Lisp; -*-
 
 (cl:defpackage #:trivial-project-pathname-tests
-  (:use #:cl #:alexandria #:anaphora #:clunit #:let-plus))
+  (:use #:cl #:alexandria #:anaphora #:clunit #:let-plus)
+  (:export
+   #:run-suite))
 
 (in-package #:trivial-project-pathname-tests)
+
+(defsuite tests ())
+
+(defun run (&optional interactive?)
+  "Run the unit tests."
+  (run-suite 'tests :use-debugger interactive?))
